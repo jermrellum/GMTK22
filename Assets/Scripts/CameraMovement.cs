73a00909394d744decfe23e09d918a8ad1c38564
jrm10008 <joseph.muller@uconn.edu>
2022-07-12 18:14:17 -0400

    Trying to fix the git repo

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,11 +6,25 @@ public class CameraMovement : MonoBehaviour
 {
     public float moveSpeed = 0.05f;
 
+    [SerializeField] private float borderBottom = 0.0f;
+    [SerializeField] private float borderLeft = 0.0f;
+    [SerializeField] private float borderRight = 60.0f;
+    [SerializeField] private float borderTop = 60.0f;
+
+    private float minMax(float val, float min, float max)
+    {
+        //int umin = std::min(min, max);
+        //int umax = std::max(min, max);
+
+        return System.Math.Min(System.Math.Max(min, val), max);
+    }
+
     void Update()
     {
         float horIn = Input.GetAxis("Horizontal");
         float verIn = Input.GetAxis("Vertical");
 
-        this.transform.position = new Vector3(transform.position.x + horIn * moveSpeed, transform.position.y, transform.position.z + verIn * moveSpeed);
+        this.transform.position = new Vector3(minMax(transform.position.x + horIn * moveSpeed, borderLeft, borderRight), transform.position.y, 
+            minMax(transform.position.z + verIn * moveSpeed, borderBottom, borderTop));
     }
 }
