cb2c55c2a4180d3fccdbc4c2e0c36e27a5784890
jrm10008 <joseph.muller@uconn.edu>
2022-07-12 17:17:13 -0400

    Adding new gitignore

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private GameObject button;
 
     private new Renderer renderer;
 
@@ -25,5 +27,17 @@ public class Tile : MonoBehaviour
     private void OnMouseExit()
     {
         renderer.material.color = Color.white;
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            GameObject newCanvas = Instantiate(canvas) as GameObject;
+            GameObject newButton = Instantiate(button) as GameObject;
+            newButton.transform.SetParent(newCanvas.transform, false);
+            newButton.transform.position = Input.mousePosition;
+        }
+
     }
 }
